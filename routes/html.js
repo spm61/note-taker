e647358c4b5ba611b918e59e42d6b7b4e4f7c087
spmoroney <spmoroney@att.net>
2023-09-17 16:58:11 -0400

    udpate HTML routes

--- routes/html.js
+++ routes/html.js
@@ -4,12 +4,12 @@ const html = require('express').Router();
 
 //The Routing
 //If we want the notes file that's what we should get.
-app.get('/notes', (req, res) => {
+html.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-//Return the base index.html file if no other routes exist. 
-app.get('*', (req, res) => {
+//Return the base index.html file in the event that anything that isn't a route is supplied. 
+html.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
